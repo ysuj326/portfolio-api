adaefeef166a2a50c267ea63a3c36250d70aed68
tdi-sj <sj326@nsmg21.com>
2022-12-21 20:18:51 +0900

    deploy to gh-pages

---
+++ config/config.js
@@ -0,0 +1,4 @@
+export const prefix =
+  process.env.NODE_ENV === "production"
+    ? "https://ysuj326.github.io/hyemi-portfolio"
+    : "";
--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    loader: 'akamai',
+    path: '',
+  },
+  assetPrefix:
+    process.env.NODE_ENV === "production"
+      ? "https://ysuj326.github.io/portfolio-api"
+      : "",
 }
 
 module.exports = nextConfig
